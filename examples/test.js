5515ca49635b3808cbaff01dfad24d573c7b01e2
Simon Boudrias <simon.boudrias@cossette.com>
2013-07-26 17:16:56 -0400

    Refactor folder organisation and play a bit with the code

---
+++ examples/test.js
@@ -0,0 +1,10 @@
+var Tree = require("../lib/tree");
+var codeStr = 'var a = "bar", b = "foo";';
+
+// Test code
+var ast = new Tree(codeStr);
+
+ast.var("a").value("a");
+ast.update();
+
+console.log( ast.toString() );
