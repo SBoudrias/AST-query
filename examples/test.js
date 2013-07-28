fbc5b327de2a1f15aff8dcf4f34078fcdd5209de
Simon Boudrias <admin@simonboudrias.com>
2013-07-27 22:33:13 -0400

    Prototype the var API and first proof of concept of the code design

--- examples/test.js
+++ examples/test.js
@@ -3,8 +3,24 @@ var codeStr = 'var a = "bar", b = "foo";';
 
 // Test code
 var ast = new Tree(codeStr);
+console.log( ast.toString() + "\n\n" );
 
 ast.var("a").value("'a'");
-ast.update();
+console.log( ast.toString() + "\n\n" );
 
+ast.var("a").replaceWith("a = 'foo'");
+console.log( ast.toString() + "\n\n" );
+
+ast.var("a").insertBefore("function before() {}\n");
+console.log( ast.toString() + "\n\n" );
+
+ast.var("b").insertAfter("\nfunction after() {}");
+console.log( ast.toString() + "\n\n" );
+
+ast.var("b").rename("easy");
+console.log( ast.toString() + "\n\n" );
+
+ast.var("easy").delete();
 console.log( ast.toString() );
+
+
