93a3ad4a915ad07c939ff9e7288dbb5e2a5ac5a5
Simon Boudrias <admin@simonboudrias.com>
2014-03-29 22:59:36 -0400

    Expose program body on the top node level

--- test/tree.js
+++ test/tree.js
@@ -1,11 +1,19 @@
 var assert = require('assert');
 var Tree = require('..');
+var Body = require('../lib/nodes/Body');
 
 describe('Tree', function () {
   describe('#toString()', function () {
     it('return the generated source code', function () {
       var tree = new Tree('var a = 1');
       assert.equal(tree.toString(), 'var a = 1;');
+    });
+  });
+
+  describe('#body', function () {
+    it('is a Body node instance', function () {
+      var tree = new Tree('var a = 1');
+      assert(tree.body instanceof Body);
     });
   });
 });
