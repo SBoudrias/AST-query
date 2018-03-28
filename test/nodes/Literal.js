b6125aab359fbbdbf51fd537ffc96ad245d3c9c8
William Rusnack <williamrusnack@gmail.com>
2018-03-28 16:19:12 -0400

    Reduced whitespace

--- test/nodes/Literal.js
+++ test/nodes/Literal.js
@@ -12,17 +12,19 @@ var Tree = require('../..');
     return Tree.fromTree(tree.tree, arguments[1], arguments[2], arguments[3], arguments[4]);
   },
 ].forEach(function(program) {
-  describe('Literal nodes', function () {
-    beforeEach(function () {
-      this.tree = program('var a = 2;');
-    });
 
-    describe('#value', function () {
-      it('overwrite value', function () {
-        var literal = this.tree.var('a').value();
-        literal.value('{ a: 2 }');
-        assert.equal(this.tree.toString(), 'var a = { a: 2 };');
-      });
+describe('Literal nodes', function () {
+  beforeEach(function () {
+    this.tree = program('var a = 2;');
+  });
+
+  describe('#value', function () {
+    it('overwrite value', function () {
+      var literal = this.tree.var('a').value();
+      literal.value('{ a: 2 }');
+      assert.equal(this.tree.toString(), 'var a = { a: 2 };');
     });
   });
+});
+
 });
--- test/nodes/ObjectExpression.js
+++ test/nodes/ObjectExpression.js
@@ -13,54 +13,56 @@ var Tree = require('../..');
     return Tree.fromTree(tree.tree, arguments[1], arguments[2], arguments[3], arguments[4]);
   },
 ].forEach(function(program) {
-  describe('ObjectExpression objects', function () {
-    beforeEach(function () {
-      this.obj = new ObjectExpression(valueFactory.create('{ a: "b", foo: 1, bar: { sub: 1 }, "special-key":"foo" }'));
+
+describe('ObjectExpression objects', function () {
+  beforeEach(function () {
+    this.obj = new ObjectExpression(valueFactory.create('{ a: "b", foo: 1, bar: { sub: 1 }, "special-key":"foo" }'));
+  });
+
+  it('#type equal ObjectExpression', function () {
+    assert.equal(this.obj.type, 'ObjectExpression');
+  });
+
+  describe('#key()', function () {
+    it('get key value', function () {
+      assert.equal(this.obj.key('a').value(), 'b');
     });
 
-    it('#type equal ObjectExpression', function () {
-      assert.equal(this.obj.type, 'ObjectExpression');
+    it('returns a wrapped value', function () {
+      assert(this.obj.key('a') instanceof Literal);
+      assert(this.obj.key('bar') instanceof ObjectExpression);
+      assert(this.obj.key('bar').key('sub') instanceof Literal);
     });
 
-    describe('#key()', function () {
-      it('get key value', function () {
-        assert.equal(this.obj.key('a').value(), 'b');
-      });
-
-      it('returns a wrapped value', function () {
-        assert(this.obj.key('a') instanceof Literal);
-        assert(this.obj.key('bar') instanceof ObjectExpression);
-        assert(this.obj.key('bar').key('sub') instanceof Literal);
-      });
-
-      it('create a placeholder object if key doesn\'t exist', function () {
-        var obj = program('var b = { a: null };');
-        obj.var('b').value().key('c').value('1');
-        assert.equal(obj.toString(), 'var b = {\n    a: null,\n    c: 1\n};');
-      });
-
-      it('doesn\'t render the placeholder if no value is assigned', function () {
-        var obj = program('var b = { a: null };');
-        obj.var('b').value().key('c');
-        assert.equal(obj.toString(), 'var b = { a: null };');
-      });
-
-      it('can search for special keys',function(){
-        assert(this.obj.key('"special-key"')  instanceof Literal);
-      });
+    it('create a placeholder object if key doesn\'t exist', function () {
+      var obj = program('var b = { a: null };');
+      obj.var('b').value().key('c').value('1');
+      assert.equal(obj.toString(), 'var b = {\n    a: null,\n    c: 1\n};');
     });
 
-    describe('#value()', function () {
-      it('replace itself with new value', function () {
-        var tree = program('var b = { a: "b" };');
-        tree.var('b').value().value('1');
-        assert.equal(tree.toString(), 'var b = 1;');
-      });
+    it('doesn\'t render the placeholder if no value is assigned', function () {
+      var obj = program('var b = { a: null };');
+      obj.var('b').value().key('c');
+      assert.equal(obj.toString(), 'var b = { a: null };');
+    });
 
-      it('return the new value', function () {
-        var obj = this.obj.key('bar').value('"a"');
-        assert(obj instanceof Literal);
-      });
+    it('can search for special keys',function(){
+      assert(this.obj.key('"special-key"')  instanceof Literal);
     });
   });
+
+  describe('#value()', function () {
+    it('replace itself with new value', function () {
+      var tree = program('var b = { a: "b" };');
+      tree.var('b').value().value('1');
+      assert.equal(tree.toString(), 'var b = 1;');
+    });
+
+    it('return the new value', function () {
+      var obj = this.obj.key('bar').value('"a"');
+      assert(obj instanceof Literal);
+    });
+  });
+});
+
 });
